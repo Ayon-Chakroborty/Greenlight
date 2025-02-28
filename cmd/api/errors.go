f79606ca008c7c2497024f4bdfec921505b1041e
Ayon-Chakroborty <gv5072@wayne.edu>
2025-02-27 19:10:58 -0500

    Added json validation for client requests

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -46,7 +46,9 @@ func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
-
+func(app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string){
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
 
 
 
