4cb09c252b89b51f7fce386b229a2ff67954626a
Ayon-Chakroborty <gv5072@wayne.edu>
2025-03-25 23:20:54 -0400

    added update movie functionality

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -77,3 +77,66 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request){
+	// Get ID from URL
+	id, err := app.readIDParams(r)
+	if err != nil{
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// Get the movie record from the database to check if it exists
+	movie, err := app.models.Movies.Get(id)
+	if err != nil{
+		switch{
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// input strcut to hold JSON data from client
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	// read JSON and store it in struct
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy values from the request to the movie struct
+	movie.Title = input.Title
+	movie.Year = input.Year
+	movie.Runtime = input.Runtime
+	movie.Genres = input.Genres
+	
+	// Validate the movie struct
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid(){
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// update the record in the database
+	err = app.models.Movies.Update(movie)
+	if err != nil{
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// write updated movie as a JSON response back to the user
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie":movie}, nil)
+	if err != nil{
+		app.serverErrorResponse(w, r, err)
+	}
+}
