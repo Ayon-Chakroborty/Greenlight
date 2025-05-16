1bfb3a958aefdec81cac919908af1b533e0eca1b
Ayon-Chakroborty <gv5072@wayne.edu>
2025-05-15 22:05:40 -0400

    can now send activation tokens to user

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"greenlight.ayonchakroborty.net/internal/data"
 	"greenlight.ayonchakroborty.net/internal/validator"
@@ -52,9 +53,20 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	// Send confirmation email
 	app.background(func() {
-		err := app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		data := map[string]any{
+			"acitvationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+		
+		err := app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.Error(err.Error())
 			return
--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,10 +1,14 @@
 package data
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/sha256"
+	"database/sql"
 	"encoding/base32"
 	"time"
+
+	"greenlight.ayonchakroborty.net/internal/validator"
 )
 
 const (
@@ -19,26 +23,74 @@ type Token struct {
 	Scope     string
 }
 
-func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error){
+func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
 	token := &Token{
 		UserID: userID,
 		Expiry: time.Now().Add(ttl),
-		Scope: scope,
+		Scope:  scope,
 	}
 
 	randomBytes := make([]byte, 16)
 
 	_, err := rand.Read(randomBytes) // fill randomBytes with random bytes
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
 	// endcode with no padding
 	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
 
-	// encrypt 
+	// encrypt
 	hash := sha256.Sum256([]byte(token.Plaintext))
 	token.Hash = hash[:] // copy hash to token.Hash
 
 	return token, nil
+}
+
+// Check that plaintext token is 26 bytes long
+func ValidateTokenPlaintext(v *validator.Validator, tokenPlaintest string) {
+	v.Check(tokenPlaintest != "", "token", "must be provided")
+	v.Check(len(tokenPlaintest) == 26, "token", "must be 26 bytes long")
+}
+
+type TokenModel struct {
+	DB *sql.DB
+}
+
+// Method to generate new token and insert into database
+func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error){
+	token, err := generateToken(userID, ttl, scope)
+	if err != nil{
+		return nil, err
+	}
+
+	err = m.Insert(token)
+	return token, err
+}
+
+func (m TokenModel) Insert(token *Token) error{
+	query := `
+		INSERT INTO tokens (hash, user_id, expiry, scope)
+		VALUES ($1, $2, $3, $4)`
+
+	args := []any{token.Hash, token.UserID, token.Expiry, token.Scope}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, args...)
+	return err
+}
+
+// deletes all tokens for a specific user and scope
+func (m TokenModel) DeleteAllForUser(scope string, userID int64) error{
+	query := `
+		DELETE FROM tokens
+		WHERE scope = $1 and user_id = $2`
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, scope, userID)
+	return err
 }
